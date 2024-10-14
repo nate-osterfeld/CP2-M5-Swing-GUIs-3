b2070e9e7eb22442db30aa35789b06aebbd00061
Nate Osterfeld <42900510+NateOsterfeld@users.noreply.github.com>
2024-10-14 00:01:07 -0400

    Final commit

---
+++ src/RockPaperScissorsFrame.java
@@ -0,0 +1,175 @@
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.util.Random;
+public class RockPaperScissorsFrame extends JFrame {
+
+    JPanel mainPnl;
+    JPanel titlePnl;
+    JPanel decidePnl;
+    JPanel statsPnl;
+    JPanel winnerPnl;
+    JLabel titleLbl;
+    JButton rockBtn;
+    JButton paperBtn;
+    JButton scissorsBtn;
+    JButton quitBtn;
+    ImageIcon rockImg;
+    ImageIcon paperImg;
+    ImageIcon scissorsImg;
+
+    JLabel playerWinsL;
+    JLabel computerWinsL;
+    JLabel tiesL;
+    JTextField statsTF;
+    int playerWins;
+    int computerWins;
+    int ties;
+
+    JTextArea displayTA;
+    JScrollPane scroller;
+
+    Random rnd = new Random();
+
+    public RockPaperScissorsFrame() {
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new GridLayout(4, 1));
+
+        createTitlePnl();
+        mainPnl.add(titlePnl);
+
+        createDecidePnl();
+        mainPnl.add(decidePnl);
+
+        createStatsPnl();
+        mainPnl.add(statsPnl);
+
+        createWinnerPnl();
+        mainPnl.add(winnerPnl);
+
+        add(mainPnl);
+        setSize(700, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+    private void createTitlePnl() {
+        titlePnl = new JPanel();
+
+        titleLbl = new JLabel("Rock Paper Scissors Game");
+
+        titleLbl.setVerticalTextPosition(JLabel.BOTTOM);
+        titleLbl.setHorizontalTextPosition(JLabel.CENTER);
+
+        titlePnl.add(titleLbl);
+    }
+
+    private void createDecidePnl() {
+        decidePnl = new JPanel();
+        decidePnl.setLayout(new GridLayout(1, 4));
+        decidePnl.setBorder(new TitledBorder(new EtchedBorder(), "Pick"));
+
+        rockImg = new ImageIcon("src/rockIMG.png");
+        paperImg = new ImageIcon("src/paperIMG.png");
+        scissorsImg = new ImageIcon("src/scissorsIMG.png");
+
+        rockBtn = new JButton(rockImg);
+        paperBtn = new JButton(paperImg);
+        scissorsBtn = new JButton(scissorsImg);
+        quitBtn = new JButton("Quit");
+
+        decidePnl.add(rockBtn);
+        decidePnl.add(paperBtn);
+        decidePnl.add(scissorsBtn);
+        decidePnl.add(quitBtn);
+
+        rockBtn.addActionListener((ActionEvent ae) -> {
+            displayTA.append("You play: Rock\n");
+            play(0);
+        });
+
+        paperBtn.addActionListener((ActionEvent ae) -> {
+            displayTA.append("You play: Paper\n");
+            play(1);
+        });
+
+        scissorsBtn.addActionListener((ActionEvent ae) -> {
+            displayTA.append("You play: Scissors\n");
+            play(2);
+        });
+
+        quitBtn.addActionListener((ActionEvent ae) -> System.exit(0));
+    }
+
+
+    private void createStatsPnl() {
+        statsPnl = new JPanel();
+
+        playerWinsL = new JLabel("Players Wins: 0");
+        computerWinsL = new JLabel("Computers Wins: 0");
+        tiesL = new JLabel("Ties: 0");
+
+        statsPnl.add(playerWinsL);
+        statsPnl.add(computerWinsL);
+        statsPnl.add(tiesL);
+
+        statsTF = new JTextField(15);
+    }
+
+
+    private void createWinnerPnl() {
+        winnerPnl = new JPanel();
+        displayTA = new JTextArea(10, 35);
+        displayTA.setEditable(false);
+        scroller = new JScrollPane(displayTA);
+        winnerPnl.add(scroller);
+    }
+
+    private void play(int playerMove) {
+        int computerMove = rnd.nextInt(3);
+
+
+        if (playerMove == computerMove) {
+            if (playerMove == 0) {
+                displayTA.append("Computer plays: Rock\n It's a tie!\n");
+            }
+            else if (playerMove ==1) {
+                displayTA.append("Computer plays: Paper\n It's a tie!\n");
+            }
+            else {
+                displayTA.append("Computer plays: Scissors\n It's a tie!\n");
+            }
+            ties++;
+            tiesL.setText("Ties: " + ties);
+        }
+        else if (playerMove == 1 && computerMove == 0) {
+            displayTA.append("Computer plays: Rock\n Paper covers Rock (Player Wins)\n");
+            playerWins++;
+        }
+        else if (playerMove == 0 && computerMove == 1) {
+            displayTA.append("Computer plays: Paper\n Paper covers Rocker (Computer Wins)\n");
+            computerWins++;
+        }
+        else if (playerMove == 2 && computerMove == 0) {
+            displayTA.append("Computer plays: Rock\n Rock breaks Scissors (Computer Wins)\n");
+            computerWins++;
+        }
+        else if (playerMove == 0 && computerMove == 2) {
+            displayTA.append("Computer plays: Scissors\n Rock breaks Scissors (Player Wins)\n");
+            playerWins++;
+        }
+        else if (playerMove == 2 & computerMove == 1) {
+            displayTA.append("Computer plays: Paper\n Scissors cuts Paper (Player Wins)\n");
+            playerWins++;
+        }
+        else if (playerMove == 1 && computerMove == 2) {
+            displayTA.append("Computer plays: Scissors\n Scissors cuts Paper (Computer Wins)\n");
+            computerWins++;
+        }
+        playerWinsL.setText("Player Wins: " + playerWins);
+        computerWinsL.setText("Computer Wins: " + computerWins);
+    }
+}
